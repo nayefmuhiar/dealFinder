0ee04dba1534858b98073a94de6854177390bc26
Nayef Muhiar <nayef@buttercloud.com>
2018-03-09 18:21:03 +0200

    Rename method to better reflect functionality

--- lib/api_consumer.rb
+++ lib/api_consumer.rb
@@ -3,7 +3,7 @@ class ApiConsumer
   BASE_URI = "https://offersvc.expedia.com/offers/v2/getOffers"
   OPTIONS = {scenario: "deal-finder", page: "foo", uid: "foo", productType: "Hotel"}
 
-  def self.get_all_deals(options= {})
+  def self.get_deals(options= {})
     params = {query:  (options.blank? ? ApiConsumer::OPTIONS : ApiConsumer::OPTIONS.merge(options))}
     response = HTTParty.get(ApiConsumer::BASE_URI, params)
     response.parsed_response
