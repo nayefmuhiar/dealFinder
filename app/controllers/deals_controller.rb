d23d966a0c1dea363deb0ec50c7637dacd1edc43
Nayef Muhiar <nayef@buttercloud.com>
2018-03-09 18:24:25 +0200

    Edit index method in deals controller to consume API and send data to view

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,6 +1,8 @@
 class DealsController < ApplicationController
 
   def index
-    
+    result = ApiConsumer.get_deals
+    @deal_info = result["offerInfo"]
+    @deals = result["offers"]["Hotel"]
   end
 end
