56b75e7e6a0fd880d92a6d085a379660b3f41ed4
Nayef Muhiar <nayef@buttercloud.com>
2018-03-09 23:01:42 +0200

    Add logic to filter data in the deals controller, add route for find action in deals controllers

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,8 +1,30 @@
 class DealsController < ApplicationController
 
+
   def index
     result = ApiConsumer.get_deals
+    set_result(result)
+  end
+
+  def find
+    result = ApiConsumer.get_deals(filter_blank_params_and_return_hash(deal_params))
+    set_result(result)
+  end
+
+  private
+
+  def set_result(result)
     @deal_info = result["offerInfo"]
     @deals = result["offers"]["Hotel"]
   end
+
+  def filter_blank_params_and_return_hash(params)
+    params.select {|k,v| v.present?}.to_h
+  end
+
+  def deal_params
+    params.require(:deal).permit([:destinationName, :destinationCity, :regionIds, :minTripStartDate, :maxTripStartDate, :lengthOfStay, :minStarRating,
+      :maxStarRating, :minTotalRate, :maxTotalRate, :minQuestRating, :maxQuestRating])
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'deals#index'
+
+  post "/find", to: "deals#find"
+  get "/find", to: "deals#index" #so it won't crash when there is no params on refresh
 end
