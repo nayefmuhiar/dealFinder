604deb4cab9c3e08738d31d3ee8099e97066d23d
Nayef Muhiar <nayef@buttercloud.com>
2018-03-09 14:28:52 +0200

    Fix typo in ApiConsumer class

--- lib/api_consumer.rb
+++ lib/api_consumer.rb
@@ -1,7 +1,7 @@
 class ApiConsumer
   include HTTParty
   BASE_URI = "https://offersvc.expedia.com/offers/v2/getOffers"
-  OPTIONS = {scenario: "deal-finder", page: "foo", uid: "food", productType: "Hotel"}
+  OPTIONS = {scenario: "deal-finder", page: "foo", uid: "foo", productType: "Hotel"}
 
   def self.get_all_deals(options= {})
     params = {query:  (options.blank? ? ApiConsumer::OPTIONS : ApiConsumer::OPTIONS.merge(options))}
